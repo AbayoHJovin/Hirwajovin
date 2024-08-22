5bfeb479d69da254b46731989c84e920f5e80bfe
AbayoHJovin <abayohirwajovin@gmail.com>
2024-08-22 12:00:51 +0200

    fixed an error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,3 @@
-// import type { Metadata } from "next";
-// import "./globals.css";
-// import Navbar from "@/components/navbar";
-// import Footer from "@/components/Footer";
-
-// export const metadata: Metadata = {
-//   title: "Abayo Hirwa jovin",
-//   description: "jovix",
-// };
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <html lang="en">
-//       <head>
-//         <link rel="icon" href="./favicon.ico" />
-//       </head>
-//       <body className="bg-white dark:bg-black">
-//         <Navbar />
-//         <main className="relative overflow-hidden">{children}</main>
-//         <Footer />
-//       </body>
-
-//     </html>
-//   );
-// }
-
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/navbar";
@@ -35,13 +5,14 @@ import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Abayo Hirwa Jovin - Portfolio",
-  description: "Welcome to A.H.Jovin's portifolio",
+  description: "Welcome to A.H. Jovin's portfolio",
   keywords: "Abayo Hirwa Jovin, Hirwa, Jovin, web developer, portfolio",
-  authors: "Abayo Hirwa Jovin",
+  authors: [
+    { name: "Abayo Hirwa Jovin", url: "https://hirwajovin.vercel.app" },
+  ],
   openGraph: {
     title: "Abayo Hirwa Jovin - Web Developer",
-    description:
-      "Explore the portfolio of Abayo Hirwa Jovin, featuring projects in Next.js, React, and more.",
+    description: "Explore the portfolio of Abayo Hirwa Jovin, featuring projects in Next.js, React, and more.",
     url: "https://hirwajovin.vercel.app",
     type: "website",
     images: [
@@ -53,27 +24,28 @@ export const metadata: Metadata = {
       },
     ],
   },
+
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="./favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
         <meta
           name="keywords"
-          content="Abayo Hirwa Jovin, Hirwa, Jovin, web developer, portfolio"
+          content="Abayo Hirwa Jovin,Abayo,Abayo Hirwa, Hirwa, Jovin, web developer, portfolio"
         />
         <meta name="author" content="Abayo Hirwa Jovin" />
         <meta property="og:title" content="Abayo Hirwa Jovin - Web Developer" />
         <meta
           property="og:description"
-          content="Explore the portfolio of Abayo Hirwa Jovin and more."
+          content="Explore the portfolio of Abayo Hirwa Jovin"
         />
         <meta property="og:url" content="https://hirwajovin.vercel.app" />
         <meta property="og:type" content="website" />
